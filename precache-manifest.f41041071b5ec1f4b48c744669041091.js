6a9af7fb72f0b23c02732e223790310dc107ca3a
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-10-27 16:12:52 +0530

    Updates

---
+++ precache-manifest.f41041071b5ec1f4b48c744669041091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce29d298d8e00370b29436059217b73",
+    "url": "/Mini-Digital-Clock/index.html"
+  },
+  {
+    "revision": "e7b28bb9fc45373f7f5f",
+    "url": "/Mini-Digital-Clock/static/css/main.683f41f4.chunk.css"
+  },
+  {
+    "revision": "8b5d9402451094fd5f2f",
+    "url": "/Mini-Digital-Clock/static/js/2.02d57afb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Mini-Digital-Clock/static/js/2.02d57afb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b28bb9fc45373f7f5f",
+    "url": "/Mini-Digital-Clock/static/js/main.13cc32b5.chunk.js"
+  },
+  {
+    "revision": "f2abd0a7c8a8939e1765",
+    "url": "/Mini-Digital-Clock/static/js/runtime-main.ef8aca2b.js"
+  }
+]);
